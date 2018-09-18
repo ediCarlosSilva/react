0f6930bc3600a1d86ba2ddac95f906773ffe9a76
Edi Carlos note <edi_csilva@hotmail.com>
2018-09-17 23:53:40 -0300

    criado componente para inputs, enviando requisiçao ajax

--- src/App.js
+++ src/App.js
@@ -2,24 +2,60 @@ import React, { Component } from 'react';
 import './css/pure-min.css';
 import './css/side-menu.css';
 import $ from 'jquery';
+import InputCustomizado from './componentes/InputCustomizado';
+import BotaoSubmitCustomizado from './componentes/BotaoSubmitCustomizado';
 
 class App extends Component {
 
   constructor() {
     super();
-    this.state = {lista: []};
+    this.state = {lista: [], nome: '', email: '', senha: ''};
+    this.enviaForm = this.enviaForm.bind(this);
+    this.setNome = this.setNome.bind(this);
+    this.setEmail = this.setEmail.bind(this);
+    this.setSenha = this.setSenha.bind(this);
   }
 
   componentDidMount() {
-    console.log('DidMount');
+    
     $.ajax({
       url:"http://localhost:8080/api/autores",
       datatype: 'json',
       success: function(resposta) {
-        console.log('Chegou a resposta.');
+        
         this.setState({lista: resposta});
       }.bind(this)
     })
+  }
+
+  enviaForm(evento) {
+    evento.preventDefault();
+    
+    $.ajax({
+      url: 'http://localhost:8080/api/autores',
+      contentType: 'application/json',
+      dataType: 'json',
+      type: 'post',
+      data: JSON.stringify({nome: this.state.nome, email: this.state.email, senha: this.state.senha}),
+      success: function(resposta) {
+        this.setState({lista: resposta})
+      }.bind(this),
+      error: function(resposta) {
+        console.log('erro');
+      }
+    });
+  }
+
+  setNome(evento) {
+    this.setState({nome: evento.target.value});
+  }
+
+  setEmail(evento) {
+    this.setState({email: evento.target.value});
+  }
+
+  setSenha(evento) {
+    this.setState({senha: evento.target.value});
   }
 
   render() {
@@ -33,12 +69,12 @@ class App extends Component {
 
         <div id="menu">
           <div className="pure-menu">
-            <a href="#" className="pure-menu-heading">Company</a>
+            <a href="" className="pure-menu-heading">Company</a>
 
             <ul className="pure-menu-list">
-              <li className="pure-menu-item"><a href="#" className="pure-menu-link">Home</a></li>
-              <li className="pure-menu-item"><a href="#" className="pure-menu-link">Autor</a></li>
-              <li className="pure-menu-item"><a href="#" className="pure-menu-link">Livro</a></li>
+              <li className="pure-menu-item"><a href="" className="pure-menu-link">Home</a></li>
+              <li className="pure-menu-item"><a href="" className="pure-menu-link">Autor</a></li>
+              <li className="pure-menu-item"><a href="" className="pure-menu-link">Livro</a></li>
             </ul>
 
           </div>
@@ -50,23 +86,13 @@ class App extends Component {
           </div>
           <div className="content" id="content">
             <div className="pure-form pure-form-aligned">
-              <form className="pure-form pure-form-aligned">
-                <div className="pure-control-group">
-                  <label htmlFor="nome">nome</label>
-                  <input type="text" id="nome" name="nome" value="" />
-                </div>
-                <div className="pure-control-group">
-                  <label htmlFor="email">email</label>
-                  <input type="email" id="email" name="email" value="" />
-                </div>
-                <div className="pure-control-group">
-                  <label htmlFor="senha">Senha</label>
-                  <input type="password" id="senha" name="senha" />
-                </div>
-                <div className="pure-control-group">
-                  <label></label>
-                  <button type="submit" className="pure-button pure-button-primary">Gravar</button>
-                </div>
+              <form className="pure-form pure-form-aligned" onSubmit={this.enviaForm} method="post">
+                <InputCustomizado id="nome" type="text" name="nome" value={this.state.nome} onChange={this.setNome} label="Nome" />
+                <InputCustomizado id="email" type="email" name="email" value={this.state.email} onChange={this.setEmail} label="E-mail" />
+                <InputCustomizado id="senha" type="password" name="senha" value={this.state.senha} onChange={this.setSenha} label="Senha" />
+                
+                <BotaoSubmitCustomizado label="Gravar" />
+
               </form>
             </div>
 
