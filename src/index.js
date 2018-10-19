8eacc27fad30365844e3e95a84d61bd8e3bf874c
Edi Carlos note <edi_csilva@hotmail.com>
2018-10-19 11:27:53 -0300

    implementao tratamento de rotas com react-router@3.2.0

--- src/index.js
+++ src/index.js
@@ -2,7 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import AutorBox from './Autor';
+import Home from './Home';
+import {Router, Route, browserHistory, IndexRoute} from 'react-router';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    (<Router history={browserHistory}>
+        <Route path="/" component={App}>
+            <IndexRoute path="/" component={Home} />
+            <Route path="/autor" component={AutorBox} />
+            <Route path="/livro" />
+        </Route>
+    </Router>),
+    document.getElementById('root'));
 registerServiceWorker();
